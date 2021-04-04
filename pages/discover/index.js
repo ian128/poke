48e45ecf638d6284d0e11f4184fb3f1298f4eac4
Alessandro Ferdian <alessandroferdian128@gmail.com>
2021-04-04 11:27:40 +0700

    pokemon list

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -1,9 +1,15 @@
+
+/** @jsxRuntime classic /
+/* @jsx jsx */
+import Link from "next/link";
 import { useRouter } from 'next/router'
 import React, { useState, useEffect } from 'react';
+import { jsx, css } from '@emotion/react'
+
 import { useQuery } from "@apollo/client";
 import { GET_POKEMONS } from '../../schema/pokemon.schema';
 import { LoadingSpinner } from '../../components/loading.component';
-import Link from 'next/link'
+import { ScreenBreakpoints } from '../../styles/screenBreakpoint';
 
 const Pokemons=(props)=>{
     const [page, setPage]=useState(1)
@@ -33,17 +39,17 @@ const Pokemons=(props)=>{
     return (
         <div className="container">
             <h4>Discover Pokémons</h4>
-            {
-                list.map(item=> {
-                    return(
-                        <Link 
-                        key={Math.random()}
-                        href={`/discover/detail/${item.name}`}>
-                            <li>{item.name}</li>
-                        </Link>
-                    )
-                })
-            }
+            <div css={PokemonCardGridCss}>
+                {
+                    list.map(item =>{
+                        return (
+                            <PokemonCard
+                            key={Math.random()}
+                            pokemonData={item}></PokemonCard>
+                        )
+                    })
+                }
+            </div>
             {
                 loading ? <LoadingSpinner></LoadingSpinner>: ''
             }
@@ -58,3 +64,77 @@ const Pokemons=(props)=>{
 }
 
 export default Pokemons
+
+const PokemonCardGridCss=css`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 75%;    
+    ${ScreenBreakpoints.xs}{
+        width: 100%;
+        flex-direction: column;
+    }
+    ${ScreenBreakpoints.sm}{
+        width: 100%;
+        flex-direction: row;
+    }
+    ${ScreenBreakpoints.md}{
+        width: 75%;
+        flex-direction: row;
+    }
+`
+
+const PokemonCardCss=css`
+    margin: 4pt;
+    padding: 8pt;
+    text-align: left;
+    text-decoration: none;
+    border: 1px solid #eaeaea;
+    border-radius: 10px;
+    position: relative;
+    flex-basis: 40%;
+    cursor: pointer;
+    .header{
+        min-height: 32pt;
+    }
+    .image{
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 64pt;
+    }
+
+    .utility{
+        margin-top: 8pt;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        .time{
+            flex: 1;
+        }
+        .option{
+            text-align: right;
+            flex: 1;
+        }
+    }
+`
+
+const PokemonCard=React.forwardRef((props, ref) => {
+    const {pokemonData} = props 
+    useEffect(()=>{
+        console.log(pokemonData)
+    }, [pokemonData])
+    return (
+        <Link 
+        href={`/discover/detail/${pokemonData.name}`}>
+            <div css={ [PokemonCardCss]}>
+                <div className="header">
+                    <h2 css={css`margin-bottom: 0`}>{pokemonData.name}</h2>
+                </div>
+                {
+                    pokemonData ? <img className="image" src={pokemonData.image}></img> : ''
+                }
+            </div>
+        </Link>
+    )
+})
